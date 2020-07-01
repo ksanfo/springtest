c520771e154c759423c71decdc12156cafc4c526
ok <ksanfo@gmail.com>
2020-07-01 18:32:13 +0300

    second commit

---
+++ src/test/java/org/example/MusicPlayerTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MusicPlayerTest {
+
+    @Test
+    public void setMusic() {
+    }
+
+    @Test
+    public void playMusic() {
+    }
+}
