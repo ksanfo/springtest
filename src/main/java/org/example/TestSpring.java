3a43d981fa6bddbba0335d121eb911376e81985a
ok <ksanfo@gmail.com>
2020-07-03 15:00:45 +0300

    init method and ect.

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,15 +7,27 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+        ClassicalMusic classicalMusic =context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 
-//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class );
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
-        musicPlayer.playMusic();
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
